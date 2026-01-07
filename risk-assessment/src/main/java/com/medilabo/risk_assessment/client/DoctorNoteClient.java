8668e5de7955cbbf1ae08ae95ab27aafe65596ee
Kappa6103 <schadekevin@protonmail.com>
2026-01-07 16:09:35 +0100

    Refactored API clients: streamlined URL construction with `UriComponentsBuilder` in `DoctorNoteClient` and `PatientClient`. Removed unused imports in `RiskAssessmentController`.

--- risk-assessment/src/main/java/com/medilabo/risk_assessment/client/DoctorNoteClient.java
+++ risk-assessment/src/main/java/com/medilabo/risk_assessment/client/DoctorNoteClient.java
@@ -5,6 +5,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -12,8 +13,14 @@ import java.util.List;
 public class DoctorNoteClient extends AbstractClient {
 
     public List<DoctorNote> getAllNotesByPatientId(Integer patientId) {
+        final String url = UriComponentsBuilder
+                .fromUriString(gatewayUrl)
+                .path("/notes")
+                .queryParam("patientId",patientId)
+                .toUriString();
+
         ResponseEntity<List<DoctorNote>> response = restTemplate.exchange(
-                gatewayUrl + "/notes/patient/" + patientId,
+                url,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<>() {}
--- risk-assessment/src/main/java/com/medilabo/risk_assessment/client/PatientClient.java
+++ risk-assessment/src/main/java/com/medilabo/risk_assessment/client/PatientClient.java
@@ -2,23 +2,31 @@ package com.medilabo.risk_assessment.client;
 
 import com.medilabo.risk_assessment.model.Patient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class PatientClient extends AbstractClient {
 
+    private static final String PATIENT_PATH = "/patients";
+
     public Patient getPatientById(Integer patientId) {
-        return restTemplate.getForObject(
-                gatewayUrl + "/patient/" + patientId,
-                Patient.class
-        );
+        final String url = UriComponentsBuilder
+                .fromUriString(gatewayUrl)
+                .path(PATIENT_PATH)
+                .buildAndExpand(patientId)
+                .toUriString();
+
+        return restTemplate.getForObject(url, Patient.class);
     }
 
 
     public void savePatient(Patient patient) {
-        restTemplate.postForEntity(
-                gatewayUrl + "/patient/add",
-                patient,
-                Void.class
-        );
+        final String url = UriComponentsBuilder
+                .fromUriString(gatewayUrl)
+                .path(PATIENT_PATH)
+                .buildAndExpand(patient.getId())
+                .toUriString();
+
+        restTemplate.put(url, patient);
     }
 }
