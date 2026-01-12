6c44fe896119438cbb5c0c4d33bee47bb2b83b63
Kappa6103 <schadekevin@protonmail.com>
2026-01-12 12:00:51 +0100

    CHERRY-PICKED from the dev-auth-frontend-to-gateway branch. all good. Added integration tests and refined template configurations: - Added new WebTestClient-based test in `PatientServiceFrontApplicationTests` for `PatientClient` and REST endpoint validation. - Updated JTE source directory in `pom.xml`.

---
+++ patient-service-front/src/test/java/com/medilabo/patient_service_front/IntegrationTests.java
@@ -0,0 +1,63 @@
+package com.medilabo.patient_service_front;
+
+import com.medilabo.patient_service_front.client.PatientClient;
+import com.medilabo.patient_service_front.models.Patient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// Use RANDOM_PORT to start a real Servlet container (Tomcat)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTests {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private PatientClient patientClient;
+
+    @Value("${api.gateway.url}")
+    protected String gatewayUrl;
+
+    private static final String PATIENT_PATH = "/patients";
+
+    @Test
+    void testPatientClientIntegration() {
+        List<Patient> patients = patientClient.getAll();
+
+        assertThat(patients).isNotNull();
+    }
+
+    @Test
+    public void getAll() {
+        final String url = UriComponentsBuilder
+                .fromUriString(gatewayUrl)
+                .path(PATIENT_PATH)
+                .build()
+                .toUriString();
+
+        ResponseEntity<List<Patient>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {}
+        );
+        assertTrue(response.hasBody());
+        List<Patient> patientList = new ArrayList<>();
+        assertThat(response.getBody()).isExactlyInstanceOf(patientList.getClass());
+    }
+
+}
