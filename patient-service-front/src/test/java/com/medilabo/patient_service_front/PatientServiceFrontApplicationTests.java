295cc583397a0112f0467cc3404a656ef9bf681c
Kappa6103 <schadekevin@protonmail.com>
2026-01-12 12:00:51 +0100

    Added integration tests and refined template configurations: - Added new WebTestClient-based test in `PatientServiceFrontApplicationTests` for `PatientClient` and REST endpoint validation. - Updated JTE source directory in `pom.xml`.

--- patient-service-front/src/test/java/com/medilabo/patient_service_front/PatientServiceFrontApplicationTests.java
+++ patient-service-front/src/test/java/com/medilabo/patient_service_front/PatientServiceFrontApplicationTests.java
@@ -1,10 +1,26 @@
 package com.medilabo.patient_service_front;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest
 class PatientServiceFrontApplicationTests {
+
+	WebTestClient webClient = WebTestClient.bindToServer()
+			.baseUrl("http://localhost:8080")
+			.build();
+
+
+	@Test
+	void givenRunningService_whenGetSingleCampaign_thenExpectStatus() {
+		webClient.get()
+				.uri("/patient/list")
+				.exchange()
+				.expectStatus()
+				.isOk();
+	}
 
 	@Test
 	void contextLoads() {
